e8a22479dc1693a044d864e6357f289487420e77
Eliza B <vivalterdesigns@gmail.com>
2021-06-22 14:52:50 -0400

    Added global variables to select elements

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const guessedLetters = document.querySelector(".guessed-letters");
+                    //ul where guessed letters appear
+const guessButton = document.querySelector(".guess");
+                    //Guess Button (first)
+const inputGuess = document.querySelector(".letter");
+                    //text-input where players type their guess
+const wordInProgress = document.querySelector(".word-in-progress");
+                    //Empty p where word in progress appears
+const guessesRemaining = document.querySelector(".remaining");
+                    //p where number of remaining guesses will appear
+const numGuess = document.querySelector("span"); 
+                    //span in guessesRemaining that surrounds the number
+const message = document.querySelector(".message");
+                    //p where message will appear as user guesses
+const playButton = document.querySelector(".play-again hide");
+                    //hidden button appears to say "Play again!"
+const word = "magnolia";
+    //starting word to test game
+    
