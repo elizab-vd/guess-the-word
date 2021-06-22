80986b8577e91a575457d3709914b0344a8e680f
Eliza B <vivalterdesigns@gmail.com>
2021-06-22 15:46:31 -0400

    Added placeholders and event listener to guess btn

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const wordInProgress = document.querySelector(".word-in-progress");
                     //Empty p where word in progress appears
 const guessesRemaining = document.querySelector(".remaining");
                     //p where number of remaining guesses will appear
-const numGuess = document.querySelector("span"); 
+const numGuess = document.querySelector(".remaining span"); 
                     //span in guessesRemaining that surrounds the number
 const message = document.querySelector(".message");
                     //p where message will appear as user guesses
@@ -16,4 +16,22 @@ const playButton = document.querySelector(".play-again hide");
                     //hidden button appears to say "Play again!"
 const word = "magnolia";
     //starting word to test game
-    
+
+
+    //STILL HAVE QUESTIONS ABOUT THE CIRCLES
+const placeholder = function (word) {
+    const placeholderCircles = [];
+    for (const letter of word) {
+        console.log(letter);
+        placeholderCircles.push("●");
+    }
+    wordInProgress.innerText = placeholderCircles.join("");
+};
+
+placeholder(word);
+
+guessButton.addEventListener("click", function (e){
+    e.preventDefault();
+    const inputLetter = document.inputGuess.value;
+    console.log(inputLetter);
+})
