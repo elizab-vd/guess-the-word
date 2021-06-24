48060c9b00ddfd424229accaea26979de3908db7
Eliza B <vivalterdesigns@gmail.com>
2021-06-24 18:26:50 -0400

    Created function to check if the player won

--- js/script.js
+++ js/script.js
@@ -97,5 +97,12 @@ const showWordInProgress = function (guessedLetters) {
     }
 
     wordInProgress.innerText = revealWord.join("");
+    checkIfWin();
 };
 
+const checkIfWin = function () {
+    if(word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = '<p class="highlight">You guessed the correct word! Congrats!</p>';
+    }
+};
